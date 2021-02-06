728367ec2a52171fb65d0599759ef69c68c6fda2
Harley Rossetto <programming@hxavr.com>
2021-02-06 23:12:01 +1100

    Added: automatically deserialise inner json for objects when  list is set.

--- Unit Info/src/Macquarie/Handbook/Data/Shared/MacquarieDataResponseCollection.cs
+++ Unit Info/src/Macquarie/Handbook/Data/Shared/MacquarieDataResponseCollection.cs
@@ -3,10 +3,26 @@ using Newtonsoft.Json;
 
 namespace Macquarie.Handbook.Data.Shared
 {
-    public class MacquarieDataResponseCollection<T>
+    public class MacquarieDataResponseCollection<T> where T : MacquarieMetadata
     {
+        private List<T> _Units;
         [JsonProperty("contentlets")]
-        public List<T> Units { get; set; }
+        public List<T> Units { 
+            get {
+                return _Units;
+            }
+            set {
+                _Units = value;
+                DeserialiseObjectsInnerJson();
+            } 
+        }
+
+        protected void DeserialiseObjectsInnerJson() {
+            foreach (var unit in Units)
+            {
+                unit.DeserialiseInnerJson();
+            }
+        }
 
         public T this[int index] {
             get {

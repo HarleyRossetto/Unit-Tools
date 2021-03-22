05028d1a60fd99d737371490ed9948f12da80985
Harley Rossetto <programming@hxavr.com>
2021-03-22 22:50:44 +1100

    Added property which allows direct access to the internal HTTPClient's Timeout property so it can be easily set from outside.

--- Unit Info/src/Macquarie/Handbook/MacquarieHandbook.cs
+++ Unit Info/src/Macquarie/Handbook/MacquarieHandbook.cs
@@ -9,50 +9,55 @@ using Macquarie.Handbook.Data.Shared;
 using Macquarie.Handbook.WebApi;
 using Newtonsoft.Json;
 
-namespace Macquarie.Handbook 
+namespace Macquarie.Handbook
 {
-    public static class MacquarieHandbook 
+    public static class MacquarieHandbook
     {
-        static readonly HttpClient httpClient = new HttpClient();
+        static readonly HttpClient httpClient = new HttpClient ();
 
         static MacquarieHandbook() {
-            if (!Directory.Exists("data/"))
-                Directory.CreateDirectory("data/");
+            if (!Directory.Exists ("data/"))
+                Directory.CreateDirectory ("data/");
+        }
+
+        public static TimeSpan WebRequestTimeout {
+            get { return httpClient.Timeout; }
+            set { httpClient.Timeout = value;}
         }
 
         public static async Task<string> DownloadString(HandbookApiRequestBuilder apiRequest) {
-            return await DownloadString(apiRequest.ToString());
+            return await DownloadString (apiRequest.ToString ());
         }
-        
+
         public static async Task<string> DownloadString(string url) {
-            var response = await httpClient.GetAsync(url);
-            return await response.Content.ReadAsStringAsync();
+            var response = await httpClient.GetAsync (url);
+            return await response.Content.ReadAsStringAsync ();
         }
         public static async Task<MacquarieDataResponseCollection<T>> GetDataResponseCollection<T>(HandbookApiRequestBuilder apiRequest) where T : MacquarieMetadata {
-            return await GetDataResponseCollection<T>(apiRequest.ToString());
+            return await GetDataResponseCollection<T> (apiRequest.ToString ());
         }
 
         public static async Task<MacquarieDataResponseCollection<T>> GetDataResponseCollection<T>(string url) where T : MacquarieMetadata {
-            return DeserialiseJsonObject<MacquarieDataResponseCollection<T>>(await DownloadString(url));
+            return DeserialiseJsonObject<MacquarieDataResponseCollection<T>> (await DownloadString (url));
         }
 
         public static async Task<MacquarieDataResponseCollection<MacquarieUnit>> GetUnitCollectionResponse(UnitApiRequestBuilder apiRequest) {
-            return await GetDataResponseCollection<MacquarieUnit>(apiRequest);
+            return await GetDataResponseCollection<MacquarieUnit> (apiRequest);
         }
 
         public static T DeserialiseJsonObject<T>(string json) {
-            #if WRITE_ALL_JSON_TO_DISK
+#if WRITE_ALL_JSON_TO_DISK
                 WriteJsonToFile(json);
-            #endif
+#endif
 
-            return JsonConvert.DeserializeObject<T>(json);
+            return JsonConvert.DeserializeObject<T> (json);
         }
 
         public static async Task WriteJsonToFile(string json) {
-            await File.WriteAllTextAsync(
-                                        string.Format(
-                                                        "downloaded/{0}.json", 
-                                                        DateTime.Now.ToString("yyyy-dd-M--HH-mm-ss-fffffff")), 
+            await File.WriteAllTextAsync (
+                                        string.Format (
+                                                        "downloaded/{0}.json",
+                                                        DateTime.Now.ToString ("yyyy-dd-M--HH-mm-ss-fffffff")),
                                         json);
         }
     }

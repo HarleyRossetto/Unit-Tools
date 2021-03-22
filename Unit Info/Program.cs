0550c02c5bde6dd1ffc7181af2c4815948d5ec0f
Harley Rossetto <programming@hxavr.com>
2021-03-22 22:57:00 +1100

    Updated brace positioning to end of line.
    Added branch to Prerequisite development list function
    to provide unit code alongside its prerequiste in the
    output.

--- Unit Info/Program.cs
+++ Unit Info/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿//#define IGNORE_UNNECESSARY
+
+using System;
 using System.IO;
 using System.Threading.Tasks;
 using System.Diagnostics;
@@ -15,23 +17,25 @@ namespace Unit_Info
 {
     class Program
     {
-        async static Task Main(string[] args)
-        {
+        async static Task Main(string[] args) {
             Program program = new Program();
 
             //await program.CustomApi_XXXXXXX();
 
             //await program.CustomAPI_CourseDownloadAndTranslation();
 
-            //await program.CustomAPI_UnitDownloadAndTranslation();
+            await program.CustomAPI_UnitDownloadAndTranslation();
 
             //await program.CustomAPI_GetCourse("C000006");
 
-            //await program.CustomAPI_GetUnit("COMP1010");
+            //await program.CustomAPI_GetUnit("EDTE3010");
+            /*
+                EDTE3010 - has larger pre-requsite chain
+            */
 
             //await program.SaveListOfCourseCodesAndTitles();
 
-            await program.SaveListOfUnitCodesAndTitles();
+            //await program.SaveListOfUnitCodesAndTitles();
 
             //Downloads all units and saves a copy of only the prerequisite enrolment rules.
             //await program.CustomAPI_GetAllUnitPrerequsiteForDevelopment();
@@ -40,8 +44,7 @@ namespace Unit_Info
         /// <summary>
         /// Demonstrates Unit request API creation, data collection and access.
         /// </summary>
-        public async Task CustomAPI_GetUnit(String unitCode)
-        {
+        public async Task CustomAPI_GetUnit(String unitCode) {
             HandbookApiRequestBuilder apiRequest = new UnitApiRequestBuilder() { ImplementationYear = 2021, Code = unitCode };
             var unitCollection = await MacquarieHandbook.GetDataResponseCollection<MacquarieUnit>(apiRequest);
 
@@ -62,8 +65,7 @@ namespace Unit_Info
         /// <param name="courseCode">
         /// The course code to attempt to retreive.
         /// </param> 
-        public async Task CustomAPI_GetCourse(String courseCode)
-        {
+        public async Task CustomAPI_GetCourse(String courseCode) {
             Stopwatch sw = new Stopwatch();
 
             var apiRequest = new CourseApiRequestBuilder() { ImplementationYear = 2021, Code = courseCode };
@@ -79,7 +81,7 @@ namespace Unit_Info
                 Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine("Course retrevial & deserialisation took {0} milliseconds.", sw.ElapsedMilliseconds);
 
-                await WriteObjectToJsonFileInDataDir(course, "Test_Course_Serialisation");
+                //await WriteObjectToJsonFileInDataDir(course, "Test_Course_Serialisation");
             } else {
                 Console.WriteLine("No course with code '{0}' was found.", courseCode);
             }
@@ -89,8 +91,7 @@ namespace Unit_Info
         /// Demonstrates Course request API creation, data collection and access.
         /// Requests all 2021 courses, with a limit of 250 results.
         /// </summary>
-        public async Task CustomAPI_CourseDownloadAndTranslation()
-        {
+        public async Task CustomAPI_CourseDownloadAndTranslation() {
             Stopwatch sw = new Stopwatch();
 
             var apiRequest = new CourseApiRequestBuilder() { ImplementationYear = 2021, Limit = 250 };
@@ -128,8 +129,7 @@ namespace Unit_Info
             Console.WriteLine("{0} milliseconds for {1} unit query & deserialisation.", sw.ElapsedMilliseconds, unitCollection.Count);
         }
 
-        public async Task CustomAPI_GetAllUnitPrerequsiteForDevelopment()
-        {
+        public async Task CustomAPI_GetAllUnitPrerequsiteForDevelopment() {
             Stopwatch sw = new Stopwatch();
 
             var apiRequest = new UnitApiRequestBuilder() { ImplementationYear = 2021, Limit = 3000 };
@@ -139,27 +139,43 @@ namespace Unit_Info
             sw.Restart();
             var unitCollection = await MacquarieHandbook.GetDataResponseCollection<MacquarieUnit>(apiRequest);
 
-            var prerequisites = from enrolementRule in (from t2 in unitCollection.Collection from enrolementRules in t2.UnitData.EnrolmentRules select enrolementRules).ToList()
-                                where enrolementRule.Type.Value == "prerequisite"
-                                select enrolementRule.Description;
 
-            var grouped = prerequisites.OrderBy(g => g.Length).GroupBy(g => g.Split(' ')[0]);
+            Boolean exportWithSubjectCode = false;
 
-            await WriteObjectToJsonFileInDataDir(grouped, "Macquarie_EnrolmentRules_Order_LENGTH");
+            if (exportWithSubjectCode) {
+                var ruleAndCode = new List<Tuple<string, string>>();
+                foreach (var i in unitCollection.Collection) {
+                    foreach (var j in i.UnitData.EnrolmentRules) {
+                        if (j.Type.Value == "prerequisite") {
+                            ruleAndCode.Add(new Tuple<string, string>(i.Code, j.Description));
+                        }
+                    }
+                }
+
+                var orderedList = ruleAndCode.OrderBy(i => i.Item2.Length);
+
+                await WriteObjectToJsonFileInDataDir(orderedList, "Macquarie_EnrolmentRules_Order_LENGTH_with_TITLE");
+            } else {
+                var prerequisites = from enrolementRule in (from t2 in unitCollection.Collection from enrolementRules in t2.UnitData.EnrolmentRules select enrolementRules).ToList()
+                                    where enrolementRule.Type.Value == "prerequisite"
+                                    orderby enrolementRule.Description.Length
+                                    select enrolementRule.Description;
+
+                await WriteObjectToJsonFileInDataDir(prerequisites, "Macquarie_EnrolmentRules_Order_LENGTH_Sanitised");
+            }
+
             sw.Stop();
 
             Console.WriteLine("{0} milliseconds for {1} unit query & deserialisation.", sw.ElapsedMilliseconds, unitCollection.Count);
         }
 
-        public async Task SaveListOfUnitCodesAndTitles()
-        {
+        public async Task SaveListOfUnitCodesAndTitles() {
             var unitCodes = await GetListOfUnitCodes();
 
             await WriteObjectToJsonFileInDataDir(unitCodes, "Macquarie_Unit_Codes");
         }
 
-        public async Task<IEnumerable<IGrouping<string, MacquarieBasicItemInfo>>> GetListOfUnitCodes()
-        {
+        public async Task<IEnumerable<IGrouping<string, MacquarieBasicItemInfo>>> GetListOfUnitCodes() {
             var apiRequest = new UnitApiRequestBuilder() { ImplementationYear = 2021, Limit = 2500 };
             var unitCollection = await MacquarieHandbook.GetDataResponseCollection<MacquarieUnit>(apiRequest);
             // List<CourseBasicInfo> courseList =
@@ -173,15 +189,13 @@ namespace Unit_Info
             return query;
         }
 
-        public async Task SaveListOfCourseCodesAndTitles()
-        {
+        public async Task SaveListOfCourseCodesAndTitles() {
             var courseCodes = await GetListOfCourseCodes();
 
             await WriteObjectToJsonFileInDataDir(courseCodes, "Macquarie_Course_Codes");
         }
 
-        public async Task<IEnumerable<IGrouping<string, MacquarieBasicItemInfo>>> GetListOfCourseCodes()
-        {
+        public async Task<IEnumerable<IGrouping<string, MacquarieBasicItemInfo>>> GetListOfCourseCodes() {
             var apiRequest = new CourseApiRequestBuilder() { ImplementationYear = 2021, Limit = 250 };
             var courseCollection = await MacquarieHandbook.GetDataResponseCollection<MacquarieCourse>(apiRequest);
             // List<CourseBasicInfo> courseList =
@@ -195,8 +209,8 @@ namespace Unit_Info
             return query;
         }
 
-         private static async Task WriteObjectToJsonFileInDataDir(object obj, string fileName, Boolean humanReadable = true) {
-            var jsonString = JsonConvert.SerializeObject(obj, Formatting.Indented);
+        private static async Task WriteObjectToJsonFileInDataDir(object obj, string fileName, Boolean humanReadable = true) {
+            var jsonString = JsonConvert.SerializeObject(obj, Formatting.Indented, new JsonSerializerSettings() { NullValueHandling = NullValueHandling.Ignore});
             await File.WriteAllTextAsync(string.Format("data/{0}_{1}.json",
                                                         fileName,
                                                         DateTime.Now.ToString("yyMMdd_HHmmssfffff")),
@@ -210,8 +224,7 @@ namespace Unit_Info
         public string Title { get; set; }
         public string Department { get; set; }
 
-        public MacquarieBasicItemInfo(string pCode, string pTitle, string pDepartment)
-        {
+        public MacquarieBasicItemInfo(string pCode, string pTitle, string pDepartment) {
             this.Code = pCode;
             this.Title = pTitle;
             this.Department = pDepartment;

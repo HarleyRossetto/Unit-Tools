7b48cbbdfae7b0bf6c89413f7a8f3a3977f13109
Harley Rossetto <programming@hxavr.com>
2021-03-30 20:23:17 +1100

    Updated the embedded json converter to handle situations were we are reading local data which no longer has embedded json within it.

--- Unit Info/src/Macquarie/Handbook/Data/Converters/MacquarieEmbeddedJsonConverter.cs
+++ Unit Info/src/Macquarie/Handbook/Data/Converters/MacquarieEmbeddedJsonConverter.cs
@@ -3,14 +3,17 @@ using Newtonsoft.Json;
 using System.Diagnostics.CodeAnalysis;
 using static Macquarie.JSON.JsonSerialisationHelper;
 
-namespace Macquarie.Handbook.Data.Converters {
+namespace Macquarie.Handbook.Data.Converters
+{
     public class MacquarieEmbeddedJsonConverter<T> : JsonConverter<T>
     {
         public override T ReadJson(JsonReader reader, Type objectType, [AllowNull] T existingValue, bool hasExistingValue, JsonSerializer serializer) {
-            if (reader.Value != null)
-                return DeserialiseJsonObject<T>((string)reader.Value);
+            //If the reader.value is null than the input is not an embedded json string
+            if (reader.Value == null)
+                return serializer.Deserialize<T>(reader);
             else {
-                return default(T);
+                //Otherwise reader.Value is an embedded json string so do the conversion
+                return DeserialiseJsonObject<T>((string)reader.Value);
             }
         }
 

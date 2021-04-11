7c89d033e8022e8c4dc5613d73655caff5bc600f
Harley Rossetto <programming@hxavr.com>
2021-04-11 23:46:48 +1000

    Corrected depth increment/decrement order. Moved BuildRanges function into class.

--- Unit Info/src/Macquarie/Handbook/Helpers/Prerequisites/ParenthesesMatcher.cs
+++ Unit Info/src/Macquarie/Handbook/Helpers/Prerequisites/ParenthesesMatcher.cs
@@ -10,7 +10,14 @@ namespace Macquarie.Handbook.Helpers.Prerequisites
         //Example input
         //"(Admission to BEd(Prim) and (EDUC258 or EDUC2580) and (EDUC260 or EDUC2600) and (EDUC267 or EDUC2670)) or (130cp including (EDUC258 or EDUC2580) and (EDUC260 or EDUC2600) and (EDUC267 or EDUC2670) and (EDTE353 or EDTE3530))"
 
-        public static IEnumerable<(int Index, ParentheseType BraceType, int Depth)> GetParenthesePairings(string prerequisite) {
+        public static IEnumerable<(Range Range, int Depth)> Match(string prerequisite) {
+            //Gets a list of all the opening and closing braces in the prerequisite string.
+            var braces = GetBraces(prerequisite);
+            //Joins those braces together as a Range and return.
+            return BuildRanges(braces);
+        }
+
+        public static IEnumerable<(int Index, ParentheseType BraceType, int Depth)> GetBraces(string prerequisite) {
             //Int index, bool (true = open, false = close)
             int depth = 0;
 
@@ -18,11 +25,26 @@ namespace Macquarie.Handbook.Helpers.Prerequisites
                 char currentCharacter = prerequisite.ElementAt(strIdx);
 
                 if (currentCharacter == '(') {
-                    yield return (strIdx, ParentheseType.Open, depth);
                     depth++;
+                    yield return (strIdx, ParentheseType.Open, depth);
                 } else if (currentCharacter == ')') {
-                    depth--;
                     yield return (strIdx, ParentheseType.Close, depth);
+                    depth--;
+                }
+            }
+        }
+
+        private static IEnumerable<(Range Range, int Depth)> BuildRanges(IEnumerable<(int Index, ParentheseType BraceType, int Depth)> braces) {
+            for (int i = 0; i < braces.Count(); i++) {
+                var topElement = braces.ElementAt(i);
+                if (topElement.BraceType == ParentheseType.Open) {
+                    for (int j = i + 1; j < braces.Count(); j++) {
+                        var bottomElement = braces.ElementAt(j);
+                        if (bottomElement.BraceType == ParentheseType.Close && bottomElement.Depth == topElement.Depth) {
+                            yield return (new Range(topElement.Index, bottomElement.Index), topElement.Depth);
+                            break;
+                        }
+                    }
                 }
             }
         }

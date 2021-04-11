8a90e6ead89b07357261a93fb19d64ea955e1e62
Harley Rossetto <programming@hxavr.com>
2021-04-11 23:44:57 +1000

    Added in range checker extension method.

--- Unit Info/src/Macquarie/Handbook/Helpers/RangeExtensions.cs
+++ Unit Info/src/Macquarie/Handbook/Helpers/RangeExtensions.cs
@@ -9,5 +9,14 @@ namespace Macquarie.Handbook.Helpers {
         public static int GetLength(this Range range) {
             return (range.End.Value - range.Start.Value + 1);
         }
+
+        public static bool IsInRange(this Range range, Range other) {
+            if (range.Start.Value >= other.Start.Value) {
+                if (range.End.Value <= other.End.Value) {
+                    return true;
+                }
+            }
+            return false;
+        }
     }
 }

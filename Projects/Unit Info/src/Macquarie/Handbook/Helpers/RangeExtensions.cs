1e255577b9f0c295fcb9c3bdbafb7fd89103e8bb
Harley Rossetto <programming@hxavr.com>
2021-07-01 22:31:27 +1000

    Added new line for spacing.

--- Projects/Unit Info/src/Macquarie/Handbook/Helpers/RangeExtensions.cs
+++ Projects/Unit Info/src/Macquarie/Handbook/Helpers/RangeExtensions.cs
@@ -5,9 +5,9 @@ namespace Macquarie.Handbook.Helpers {
         public static (int start, int length) GetRangeStartAndLength(this Range range) {
             return (range.Start.Value, range.End.Value - range.Start.Value + 1);
         }
-
+    
         public static int GetLength(this Range range) {
-            return (range.End.Value - range.Start.Value + 1);
+            return range.End.Value - range.Start.Value + 1;
         }
 
         public static bool IsInRange(this Range range, Range other) {

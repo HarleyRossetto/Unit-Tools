f313e79cae326eac659c7b111031cd1c51254811
Harley Rossetto <programming@hxavr.com>
2021-12-19 13:08:30 +1100

    Property names no longer redundant. i.e. AcademicItemType => Type.

--- Projects/MQHandbookLib/src/Macquarie/Handbook/Data/Shared/AcademicItem.cs
+++ Projects/MQHandbookLib/src/Macquarie/Handbook/Data/Shared/AcademicItem.cs
@@ -7,23 +7,23 @@ using Newtonsoft.Json;
 public record AcademicItem : IdentifiableRecord
 {
     [JsonProperty("academic_item")]
-    public KeyValueIdType AcademicItemInnerId { get; init; }
+    public KeyValueIdType InnerId { get; init; }
     [JsonProperty("academic_item_type")]
-    public LabelledValue AcademicItemType { get; init; }
+    public LabelledValue Type { get; init; }
     [JsonProperty("abbr_name")]
     public string AbbreviationName { get; init; }
     [JsonProperty("academic_item_version_name")]
-    public string AcademicItemVersionName { get; init; }
+    public string VersionName { get; init; }
     [JsonProperty("academic_item_credit_points")]
-    public string AcademicItemCreditPoints { get; init; }
+    public string CreditPoints { get; init; }
     [JsonProperty("abbreviated_name_and_major")]
     public string AbbreviatedNameAndMajor { get; init; }
     [JsonProperty("academic_item_name")]
-    public string AcademicItemName { get; init; }
+    public string Name { get; init; }
     [JsonProperty("academic_item_code")]
-    public string AcademicItemCode { get; init; }
+    public string Code { get; init; }
     [JsonProperty("academic_item_url")]
-    public string AcademicItemUrl { get; init; }
+    public string Url { get; init; }
 
     [JsonProperty("parent_record")]
     public KeyValueIdType ParentRecord { get; init; }

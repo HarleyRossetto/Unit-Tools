4efac8fd43c7a6e9935f1c3bad19410e600382ef
Harley Rossetto <programming@hxavr.com>
2021-12-19 13:11:41 +1100

    Added further mappings for DTOs.

--- Projects/MQHandbookAPI/Program.cs
+++ Projects/MQHandbookAPI/Program.cs
@@ -1,11 +1,14 @@
 using Macquarie.Handbook;
 using Macquarie.Handbook.Data;
+using Macquarie.Handbook.Data.Course;
 using Macquarie.Handbook.Data.Shared;
 using Macquarie.Handbook.Data.Unit;
 using Macquarie.Handbook.Data.Unit.Prerequisites;
+using MQHandbookAPI.Models.Macquarie.Handbook.Data.Course;
 using MQHandbookAPI.Models.Macquarie.Handbook.Data.Shared;
 using MQHandbookAPI.Models.Macquarie.Handbook.Data.Unit;
 using MQHandbookAPI.Models.Macquarie.Handbook.Data.Unit.Prerequisites;
+using CourseNoteDto = MQHandbookAPI.Models.Macquarie.Handbook.Data.Course.CourseNoteDto;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -21,29 +24,72 @@ builder.Logging.AddConsole();
 
 
 builder.Services.AddSingleton<IMacquarieHandbook, MacquarieHandbook>();
-//builder.Services.AddSingleton<Unit_Info.Helpers.ILogger, HandbookLogger>();
 
+//Create all the Dto maps
 builder.Services.AddAutoMapper(cfg => {
     cfg.AllowNullCollections = true;
-    cfg.CreateMap<MacquarieUnit, UnitDTO>();
-    cfg.CreateMap<MacquarieUnitData, UnitDataDTO>();
+    cfg.CreateMap<MacquarieUnit, UnitDto>();
+    cfg.CreateMap<MacquarieUnitData, UnitDataDto>();
 
-    cfg.CreateMap<LearningOutcome, LearningOutcomeDTO>();
-    cfg.CreateMap<EnrolmentRule, EnrolmentRuleDTO>();
-    cfg.CreateMap<Assessment, AssessmentDTO>();
-    cfg.CreateMap<UnitOffering, UnitOfferingDTO>()
-        .ForMember(dest => dest.Location, opt => opt.MapFrom(src => src.Location.Label))
+    cfg.CreateMap<LearningOutcome, LearningOutcomeDto>();
+    cfg.CreateMap<EnrolmentRule, EnrolmentRuleDto>();
+    cfg.CreateMap<Assessment, AssessmentDto>();
+    
+    cfg.CreateMap<UnitOffering, UnitOfferingDto>()
+        .ForMember(dest => dest.Location, opt => opt.MapFrom(src => src.Location.Value))
         .ForMember(dest => dest.AcademicItem, opt => opt.MapFrom(src => src.AcademicItem.Value))
         .ForMember(dest => dest.AttendanceMode, opt => opt.MapFrom(src => src.AttendanceMode.Value))
         .ForMember(dest => dest.TeachingPeriod, opt => opt.MapFrom(src => src.TeachingPeriod.Value))
         .ForMember(dest => dest.StudyLevel, opt => opt.MapFrom(src => src.StudyLevel.Value));
-    cfg.CreateMap<LearningActivity, LearningActivityDTO>()
+    
+    cfg.CreateMap<LearningActivity, LearningActivityDto>()
         .ForMember(dest => dest.Activity, opt => opt.MapFrom(src => src.Activity.Value));
+    
+    cfg.CreateMap<Requisite, RequisiteDto>()
+        .ForMember(dest => dest.Type, opt => opt.MapFrom(src => src.RequisiteType.Value));
+    
+    cfg.CreateMap<ContainerRequisiteTemporaryName, RequisiteContainerDto>()
+        .ForMember(dest => dest.Connector, opt => opt.MapFrom(src => src.ParentConnector.Value));
+
+    cfg.CreateMap<MacquarieCourse, CourseDto>();
+
+    cfg.CreateMap<MacquarieCourseData, CourseDataDto>()
+        .ForMember(dest => dest.AqfLevel, opt => opt.MapFrom(src => src.AqfLevel.Label))
+        .ForMember(dest => dest.GovtSpecialCourseType, opt => opt.MapFrom(src => src.GovtSpecialCourseType.Value))
+        .ForMember(dest => dest.PartnerFaculty, opt => opt.MapFrom(src => src.PartnerFaculty.Value))
+        .ForMember(dest => dest.ExclusivelyAnExitAward, opt => opt.MapFrom(src => src.ExclusivelyAnExitAward.Label))
+        .ForMember(dest => dest.VolumeOfLearning, opt => opt.MapFrom(src => src.VolumeOfLearning.Label))
+        .ForMember(dest => dest.CourseDurationInYears, opt => opt.MapFrom(src => src.CourseDurationInYears.Label))
+        .ForMember(dest => dest.CourseValue, opt => opt.MapFrom(src => src.CourseValue.Value));
+
+    cfg.CreateMap<MacquarieCurriculumStructureData, CurriculumStructureDataDto>();
+    cfg.CreateMap<UnitGroupingContainer, UnitGroupingContainerDto>()
+        .ForMember(dest => dest.ParentRecord, opt => opt.MapFrom(src => src.ParentRecord.Value))
+        .ForMember(dest => dest.ParentRecord, opt => opt.MapFrom(src => src.ParentRecord.Value));
+
+    cfg.CreateMap<Offering, OfferingDto>()
+        .ForMember(dest => dest.AdmissionCalendar, opt => opt.MapFrom(src => src.AdmissionCalendar.Value))
+        .ForMember(dest => dest.Location, opt => opt.MapFrom(src => src.Location.Value))
+        .ForMember(dest => dest.Mode, opt => opt.MapFrom(src => src.Mode.Value))
+        .ForMember(dest => dest.Status, opt => opt.MapFrom(src => src.Status.Value));
+
+    cfg.CreateMap<AcademicItem, AcademicItemDto>()
+        .ForMember(dest => dest.Type, opt => opt.MapFrom(src => src.Type.Label));
+
+    cfg.CreateMap<OrgUnitData, OrgUnitDataDto>();
+    cfg.CreateMap<Fee, FeeDto>()
+        .ForMember(dest => dest.FeeType, opt => opt.MapFrom(src => src.FeeType.Value));
+
+    cfg.CreateMap<CourseNote, CourseNoteDto>()
+        .ForMember(dest => dest.Type, opt => opt.MapFrom(src => src.Type.Value));
+
+    cfg.CreateMap<AdmissionRequirementPoint, AdmissionRequirementPointDto>()
+       .ForMember(dest => dest.VolumeOfLearning, opt => opt.MapFrom(src => src.VolumeOfLearning.Label));
 
     cfg.CreateMap<LabelledValue, string>().ConvertUsing(src => src.Label);
     cfg.CreateMap<KeyValueIdType, string>().ConvertUsing(src => src.Value);
 
-    cfg.CreateMap<MacquarieMaterialMetadata, MaterialMetadataDTO>()
+    cfg.CreateMap<MacquarieMaterialMetadata, MaterialMetadataDto>()
         .ForMember(dest => dest.AcademicOrganisation, opt => opt.MapFrom(src => src.AcademicOrganisation.Value))
         .ForMember(dest => dest.School, opt => opt.MapFrom(src => src.School.Value))
         .ForMember(dest => dest.Type, opt => opt.MapFrom(src => src.Type.Value))
@@ -53,8 +99,7 @@ builder.Services.AddAutoMapper(cfg => {
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+if (app.Environment.IsDevelopment()) {
     app.UseSwagger();
     app.UseSwaggerUI();
     app.UseDeveloperExceptionPage();

7907feafe7deb4ff6e0dbbc493ee41ad8961f02a
Harley Rossetto <programming@hxavr.com>
2022-01-03 17:13:17 +1100

    Azure Key Vault test project

---
+++ Projects/KeyvaultTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+
+const string KEY_VAULT_URI = @"https://kvstudyplanner.vault.azure.net/";
+
+var client = new SecretClient(new Uri(KEY_VAULT_URI), new DefaultAzureCredential());
+
+const string SECRET_NAME = "cdbhandbooksecretconnectionstring";
+var secret = await client.GetSecretAsync(SECRET_NAME);
+
+System.Console.WriteLine(secret.Value);
